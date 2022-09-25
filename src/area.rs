e1e04f6c176bd6903dbe127f1e6961a06b241ac7
Mattsi <mattsi.jansky@codurance.com>
2022-09-25 15:49:38 +0100

    Constructor for test areas

--- src/area.rs
+++ src/area.rs
@@ -7,10 +7,19 @@ pub struct Area {
 }
 
 impl Area {
+    pub(crate) fn new(description: &str, inventory: Vec<Item>) -> Area {
+        Area {
+            description: String::from(description),
+            inventory: Inventory::from(inventory)
+        }
+    }
+
     pub fn meadows() -> Area {
         Area {
             description: String::from("Your feet rest upon green meadows."),
-            inventory: Inventory::from(vec![ Item { name: String::from("Potion"), item_type: ItemType::Potion } ])
+            inventory: Inventory::from(vec![
+                Item { name: String::from("Potion"), item_type: ItemType::Potion }
+            ])
         }
     }
 
@@ -27,5 +36,20 @@ impl Area {
 
     pub fn get_from_inventory(&self, index: &usize) -> Item {
         self.inventory[index].clone()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn create_new_area() {
+        let result = Area::new("my test area", vec![
+            Item { name: String::from("my test item"), item_type: ItemType::Potion }
+        ]);
+
+        assert_eq!(result.description, "my test area");
+        assert_eq!(result.inventory[&0].name, "my test item");
     }
 }
